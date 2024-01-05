9ed130c8ccd94938e17260ec4ea3d71ff7f169e5
Joanna Liana <joanna_liana@outlook.com>
2024-01-05 11:15:58 +0100

    Fix bound controllers

--- orders/src/orders.routes.ts
+++ orders/src/orders.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 
+import { asyncWrapper } from './common/middleware/asyncWrapper';
 import { OrdersController } from './orders.controller';
 
 export const ordersRoutes = (controller: OrdersController): Router => {
   const router = Router();
 
-  router.get('/:orderId', controller.get.bind(controller));
-  router.delete('/:orderId', controller.cancel.bind(controller));
-  router.post('', controller.create.bind(controller));
+  router.get('/:orderId', asyncWrapper(controller.get.bind(controller)));
+  router.delete('/:orderId', asyncWrapper(controller.cancel.bind(controller)));
+  router.post('', asyncWrapper(controller.create.bind(controller)));
 
   return router;
 };
--- payments/src/payments.routes.ts
+++ payments/src/payments.routes.ts
@@ -1,15 +1,28 @@
 import { Router } from 'express';
 
+import { asyncWrapper } from './common/middleware/asyncWrapper';
 import { PaymentsController } from './payments.controller';
 
 export const paymentsRoutes = (controller: PaymentsController): Router => {
   const router = Router();
 
-  router.post('', controller.create.bind(controller));
-  router.patch('/:paymentId/pay', controller.pay.bind(controller));
-  router.patch('/:paymentId/reject', controller.reject.bind(controller));
-  router.patch('/:paymentId/cancel', controller.cancel.bind(controller));
-  router.patch('/:paymentId/refund', controller.refund.bind(controller));
+  router.post('', asyncWrapper(controller.create.bind(controller)));
+  router.patch(
+    '/:paymentId/pay',
+    asyncWrapper(controller.pay.bind(controller))
+  );
+  router.patch(
+    '/:paymentId/reject',
+    asyncWrapper(controller.reject.bind(controller))
+  );
+  router.patch(
+    '/:paymentId/cancel',
+    asyncWrapper(controller.cancel.bind(controller))
+  );
+  router.patch(
+    '/:paymentId/refund',
+    asyncWrapper(controller.refund.bind(controller))
+  );
 
   return router;
 };
